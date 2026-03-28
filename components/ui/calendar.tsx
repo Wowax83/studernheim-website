022245314b07309a762510b11f8d64996de36363
Wowax83 <123481550+Wowax83@users.noreply.github.com>
2026-03-28 14:23:59 +0100

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,13 +54,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-       Chevron: ({ orientation }) =>
-       orientation === "left" ? (
-       <ChevronLeft className="h-4 w-4" />
-       ) : (
-       <ChevronRight className="h-4 w-4" />
-       ),
-       }}
+  IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+  IconRight: () => <ChevronRight className="h-4 w-4" />,
+    }}
       {...props}
     />
   );
