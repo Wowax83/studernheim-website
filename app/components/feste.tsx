28bc8ebb260d8bb423477f6ba21b47e755baff00
Wowax83 <123481550+Wowax83@users.noreply.github.com>
2026-03-28 23:09:17 +0100

    Update feste.tsx

--- app/components/feste.tsx
+++ app/components/feste.tsx
@@ -1,7 +1,10 @@
-import { getFeste } from '@/lib/queries'
-import FesteClient from './feste-client'
-
 export default async function Feste() {
   const feste = await getFeste()
-  return <FesteClient feste={feste} />
+
+  return (
+    <div>
+      <h1>DEBUG FESTE</h1>
+      <pre>{JSON.stringify(feste, null, 2)}</pre>
+    </div>
+  )
 }
