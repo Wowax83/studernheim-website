acd0a7564b572f889b25eacf7d08a03722114816
Wowax83 <123481550+Wowax83@users.noreply.github.com>
2026-03-28 20:06:17 +0100

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,1 +1,18 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './sanity/schemaTypes'
 
+export default defineConfig({
+  name: 'default',
+  title: 'Studernheim CMS',
+
+  projectId: 'DEIN_PROJECT_ID', // 👈 hier einsetzen!
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
