755e790384e03a1f1e8368863a056cd29b69fb2f
Wowax83 <123481550+Wowax83@users.noreply.github.com>
2026-04-05 10:27:49 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import CookieBanner from '@/components/cookie-banner'
-import Navbar from '@/components/Navbar'
+import Navbar from '@/components/navbar'
 
 export const dynamic = 'force-dynamic'
 
