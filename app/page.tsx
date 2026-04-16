c29fa1d24f4327afc30c03876de7c6f2a5e6666f
Wowax83 <123481550+Wowax83@users.noreply.github.com>
2026-04-16 16:32:58 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,36 @@
 import Hero from './components/hero'
+import NextFestHero from './components/NextFestHero'
 import SAG from './components/sag'
-import Feste from './components/feste'
+import FesteClient from './components/feste-client'
 import Vereine from './components/vereine'
 import Termine from './components/termine'
 import Ortsverwaltung from './components/ortsverwaltung'
 import ContactForm from './components/contact-form'
 import Footer from './components/footer'
 
-export default function Home() {
-  return (
-    <main className="relative">
-      <Hero />
-      <SAG />
-      <Feste />
-      <Vereine />
-      <Termine />
-      <Ortsverwaltung />
-      <ContactForm />
-      <Footer />
-    </main>
-  )
+import { getFeste } from '@/lib/queries'
+
+export default async function Home() {
+const feste = await getFeste()
+
+return ( <main className="relative"> <Hero />
+
+```
+  {/* 🔥 Countdown / nächstes Fest */}
+  <NextFestHero feste={feste} />
+
+  <SAG />
+
+  {/* 🔥 Fest-Karten */}
+  <FesteClient feste={feste} />
+
+  <Vereine />
+  <Termine />
+  <Ortsverwaltung />
+  <ContactForm />
+  <Footer />
+</main>
+```
+
+)
 }
