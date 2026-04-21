4068d22ae0b4a60f1091fff04925cfa02d2543dd
Wowax83 <123481550+Wowax83@users.noreply.github.com>
2026-04-21 08:38:08 +0200

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://studernheim.com/sitemap.xml',
+  }
+}
